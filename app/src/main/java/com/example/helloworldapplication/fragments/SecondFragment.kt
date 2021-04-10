ea1af2cce3de83af3c4249ce8b7d8537c82e3467
nvlad <venia03@mail.ru>
2021-04-10 16:05:55 +0300

    Created Main and Fist fragments.

--- app/src/main/java/com/example/helloworldapplication/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/helloworldapplication/fragments/SecondFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.helloworldapplication.R
 
 class SecondFragment : Fragment() {
@@ -16,12 +14,5 @@ class SecondFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_second, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
-
     }
 }
